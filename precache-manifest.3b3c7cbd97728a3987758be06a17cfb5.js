d742ea1928e09f7727f7cfd0dd80bef5f03f5268
Boris Osijek <borisasd@gmail.com>
2019-11-05 23:58:13 +0100

    Updates

---
+++ precache-manifest.3b3c7cbd97728a3987758be06a17cfb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54b7db717d2dbb5c1bb301aa3c70105",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7f0a3dbdc99aaf95a038",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "7f0a3dbdc99aaf95a038",
+    "url": "/robofriends/static/js/main.757ba038.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
